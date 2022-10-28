9981bb53e664703fdcab940d1fc195a387df9667
PetterZhukov <2583367982@qq.com>
2022-10-28 10:12:17 -0700

    enum变量写在一行便于注释显示

--- static_value.h
+++ static_value.h
@@ -16,31 +16,16 @@ public:
 
     //======================== 状态值 ============================
     // HTTP请求方法，这里只支持GET
-    enum METHOD
-    {
-        GET = 0,
-        POST,
-        HEAD,
-        PUT,
-        DELETE,
-        TRACE,
-        OPTIONS,
-        CONNECT
-    };
-
+    enum METHOD {GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT};
+    
     /*
         解析客户端请求时，主状态机的状态
-        CHECK_STATE_REQUESTLINE   : 当前正在分析请求行
-        CHECK_STATE_HEADER        : 当前正在分析头部字段
-        CHECK_STATE_CONTENT       : 当前正在解析请求体
+        CHECK_STATE_REQUESTLINE     : 当前正在分析请求行
+        CHECK_STATE_HEADER          : 当前正在分析头部字段
+        CHECK_STATE_CONTENT         : 当前正在解析请求体
     */
-    enum CHECK_STATE
-    {
-        CHECK_STATE_REQUESTLINE = 0,
-        CHECK_STATE_HEADER,
-        CHECK_STATE_CONTENT
-    };
-
+    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
+    
     /*
         服务器处理HTTP请求的可能结果，报文解析的结果
         NO_REQUEST          :   请求不完整，需要继续读取客户数据
@@ -52,29 +37,14 @@ public:
         INTERNAL_ERROR      :   表示服务器内部错误
         CLOSED_CONNECTION   :   表示客户端已经关闭连接了
     */
-    enum HTTP_CODE
-    {
-        NO_REQUEST,
-        GET_REQUEST,
-        BAD_REQUEST,
-        NO_RESOURCE,
-        FORBIDDEN_REQUEST,
-        FILE_REQUEST,
-        INTERNAL_ERROR,
-        CLOSED_CONNECTION
-    };
+    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
     /*
         从状态机的三种可能状态，即行的读取状态，分别表示
         LINE_OK     :   读取到一个完整的行
         LINE_BAD    :   行出错
         LINE_OPEN   :   行数据尚且不完整
     */
-    enum LINE_STATUS
-    {
-        LINE_OK = 0,
-        LINE_BAD,
-        LINE_OPEN
-    };
+    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
 
     //================== HTTP响应的状态信息 ====================
     // 定义HTTP响应的一些状态信息
